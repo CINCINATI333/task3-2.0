3f5b04a20e373bb082177a8d1ea4576752903ce7
Kirill Sudoplatov <51190223+CINCINATI333@users.noreply.github.com>
2020-02-10 17:43:33 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -45,34 +45,33 @@ for k in range(len(a)):
     date.append(a[k][3][0])
 date = unique(date)
 date.sort()
-d = [[],[],[],[],[],[],[],[],[],[]] # массив с уроками за каждый день
+d = [[] for k in range(10)] # массив с уроками за каждый день
 for k in range(len(date)):
     for i in range(len(a)):
         if a[i][3][0] == date[k]:
             d[k].append(a[i])
-event = [[],[],[],[],[],[],[],[],[],[]]
+event = [[] for k in range(len(d))] 
 for x in range(len(event)):
     for k in range(len(d[x])):
         for i in range(len(part)):
             if d[x][k][1] == part[i][0]:
                 event[x].append(part[i][1]) # - ID всех пользователей, которые были в этот день       
                 event[x] = unique(event[x])
-parttutor = [[],[],[],[],[],[],[],[],[],[]]
+parttutor = [[] for k in range((len(d)))] 
 for x in range(len(event)):
     for k in range(len(event[x])):
         for i in range(len(teach)):
             if event[x][k] == teach[i][0]: #Если ID человека совпадает с ID учителя, то он учитель)))
                 parttutor[x].append(event[x][k])
                 parttutor[x] = unique(parttutor[x])  #ID Учителей, которые преподавали в этот день (11 янв. например)
-lesday = [[],[],[],[],[],[],[],[],[],[]] #- тут будут номера события,на которых были учителя
+lesday = [[] for k in range(10)] #- тут будут номера события,на которых были учителя
 for x in range(len(lesday)):  
     for k in range(len(parttutor[x])):
         for i in range(len(part)):
-            if part[i][1] == parttutor[x][k]: #Если ID учителя, который преподавал в этот день равен другому ID, значит вытягиваем номер
-                                                                                                                        #события
+            if part[i][1] == parttutor[x][k]: #Если ID учителя, который преподавал в этот день равен другому ID, значит вытягиваем номер события
                 lesday[x].append(part[i]) #[[event],[tutorID (11.01.2020)]]
 lessons = d #[11.01.2020[LesID,event,subject,[date]],12.01.2020[]...]]
-techtutor = [[],[],[],[],[],[],[],[],[],[]]
+techtutor = [[] for k in range((len(d)))] 
 for x in range(len(lesday)):
     for k in range(len(lesday[x])):
         for i in range(len(lessons[x])):
@@ -81,7 +80,7 @@ for x in range(len(lesday)):
                     if lessons[x][i][0] == qua[g][0]:
                         techtutor[x].append(lesday[x][k][1]) #учитель
                         techtutor[x].append(qua[g][1]) #оценка
-techtutorohnespases = [[],[],[],[],[],[],[],[],[],[]]
+techtutorohnespases = [[] for k in range((len(d)))]
 for x in range(len(techtutor)):
     for k in range(1,len(techtutor[x]),2):
         if techtutor[x][k] != '':
@@ -90,7 +89,7 @@ for x in range(len(techtutor)):
 for x in range(len(techtutorohnespases)):    
     ans.write(str(x+11)+ '\n')
     for k in range(0,len(techtutorohnespases[x]),2):
-        ans.write(techtutorohnespases[x][k] + '|'+ techtutorohnespases[x][k+1] + '\n')   #Дальше в работа в экселе (для разнообразия)
+        ans.write(techtutorohnespases[x][k] + '|'+ techtutorohnespases[x][k+1] + '\n')   #Дальше в эксель (для разнообразия)
 print('done')      
 '''
 for k in range(len(les)):
